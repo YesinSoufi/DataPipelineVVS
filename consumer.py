42611e4bbe383c3e710b7fd2ef5383b6274cd25b
LeoBuc <38210453+LeoBuc@users.noreply.github.com>
2020-07-01 18:36:50 +0200

    Add files via upload

--- consumer.py
+++ consumer.py
@@ -17,12 +17,44 @@ for message in consumer:
         message = message.value.decode()
 
         root = ET.fromstring(message)
-        timtabletime = root[0][5][0][1][1][0][0][3][0].text+"h"
-        linie = root[0][5][0][1][1][1][5][0].text
-        richtung = root[0][5][0][1][1][1][11][0].text
-        estimatedtime = root[0][5][0][1][1][0][0][3][1].text+"h"
+
+
+        # DEBUG für XML Probleme
+        count = 0
+        for child in root[0][5][0][1][1][1]:
+            print(child.tag + str(count))
+            count = count + 1
+        # ElementTree für die Geplante Abfahrtszeit
+        timtabletime = root[0][5][0][1][1][0][0][3][0].text + "h"
+
+        # ElementTree für die Linie
+        if root[0][5][0][1][1][1][5].tag == '{http://www.vdv.de/trias}PublishedLineName':
+            linie = root[0][5][0][1][1][1][5][0].text
+        elif root[0][5][0][1][1][1][6].tag == '{http://www.vdv.de/trias}PublishedLineName':
+            linie = root[0][5][0][1][1][1][6][0].text
+        else:
+            print("Probleme in der XML-Antwort: PublishedLineName ist weder Child Nr. 5 oder 6")
+
+        # ElementTree für die Richtung
+        if root[0][5][0][1][1][1][11].tag == '{http://www.vdv.de/trias}DestinationText':
+            richtung = root[0][5][0][1][1][1][11][0].text
+        elif root[0][5][0][1][1][1][12].tag == '{http://www.vdv.de/trias}DestinationText':
+            richtung = root[0][5][0][1][1][1][12][0].text
+        elif root[0][5][0][1][1][1][10].tag == '{http://www.vdv.de/trias}DestinationText':
+            richtung = root[0][5][0][1][1][1][10][0].text
+        else:
+            print("Probleme bei der XML-Antwort: DestinationText ist weder Child Nr. 11 noch Nr.12 noch Nr.10")
+
+        # Element Tree für die prognostizierte Abfahrtszeit
+        if root[0][5][0][1][1][0][0][3][1].tag == '{http://www.vdv.de/trias}EstimatedTime':
+            estimatedtime = root[0][5][0][1][1][0][0][3][1].text + "h"
+        else:
+            print("Probleme bei der Echtzeit-Erfassung auf dem Server")
+
+        # ElementTree für die Haltestelle
         haltestelle = root[0][5][0][1][1][0][0][1][0].text
 
+        # Berechnung der Verspätung, Erfassung des Datums und Erstellung des Doppelcodes (ID)
         if len(estimatedtime) & len(timtabletime) == 21:
             estdt = datetime.strptime(estimatedtime, '%Y-%m-%dT%H:%M:%SZh')
             tbldt = datetime.strptime(timtabletime, '%Y-%m-%dT%H:%M:%SZh')
