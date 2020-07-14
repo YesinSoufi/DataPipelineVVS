dfdce16f4343e0c8b942994d39cf78dcf086e67c
LeoBuc <38210453+LeoBuc@users.noreply.github.com>
2020-07-14 17:40:25 +0200

    Add files via upload

---
+++ consumer2.py
@@ -0,0 +1,115 @@
+# -- coding: utf-8 --
+import xml.etree.ElementTree as ET
+import time
+from datetime import datetime
+import mysql.connector
+import mysql
+import pykafka
+from pykafka import KafkaClient
+
+client = KafkaClient(hosts="localhost:9092")
+topic = client.topics['ubahnV1']
+
+consumer = topic.get_simple_consumer()
+for message in consumer:
+    if message is not None:
+        print(message.value.decode())
+        message = message.value.decode()
+
+        print(len(message))  # 1500 Grenze
+
+        if len(message) > 1500:
+            root = ET.fromstring(message)
+
+            # DEBUG für XML Probleme
+            count = 0
+            ergebnis = 1
+            """for child in root[0][5][0][1][1][1]:
+                print(child.tag + str(count))
+                count = count + 1"""
+            while ergebnis <= 3:
+                # ElementTree für die Geplante Abfahrtszeit
+                if root[0][5][0][ergebnis][1][0][0][2].tag == '{http://www.vdv.de/trias}ServiceDeparture':
+                    timtabletime = root[0][5][0][ergebnis][1][0][0][2][0].text + "h"
+                else:
+                    print("TimetabledTime nicht verfügbar")
+
+                # ElementTree für die Linie
+                if root[0][5][0][ergebnis][1][1][5].tag == '{http://www.vdv.de/trias}PublishedLineName':
+                    linie = root[0][5][0][ergebnis][1][1][5][0].text
+                elif root[0][5][0][ergebnis][1][1][6].tag == '{http://www.vdv.de/trias}PublishedLineName':
+                    linie = root[0][5][0][ergebnis][1][1][6][0].text
+                else:
+                    print("Probleme in der XML-Antwort: PublishedLineName ist weder Child Nr. 5 oder 6")
+
+                # ElementTree für die Richtung
+                if root[0][5][0][ergebnis][1][1][11].tag == '{http://www.vdv.de/trias}DestinationText':
+                    richtung = root[0][5][0][ergebnis][1][1][11][0].text.replace("Ã¶", "ö").replace("Ã", "ß").replace("Ã¼",
+                                                                                                                "ü").replace(
+                        "Ã", "Ö").replace("Ã¤", "ä")
+                elif root[0][5][0][ergebnis][1][1][12].tag == '{http://www.vdv.de/trias}DestinationText':
+                    richtung = root[0][5][0][ergebnis][1][1][12][0].text.replace("Ã¶", "ö").replace("Ã", "ß").replace("Ã¼",
+                                                                                                                "ü").replace(
+                        "Ã", "Ö").replace("Ã¤", "ä")
+                elif root[0][5][0][ergebnis][1][1][10].tag == '{http://www.vdv.de/trias}DestinationText':
+                    richtung = root[0][5][0][ergebnis][1][1][10][0].text.replace("Ã¶", "ö").replace("Ã", "ß").replace("Ã¼",
+                                                                                                                "ü").replace(
+                        "Ã", "Ö").replace("Ã¤", "ä")
+                else:
+                    print("Probleme bei der XML-Antwort: DestinationText ist weder Child Nr. 11 noch Nr.12 noch Nr.10"+ergebnis)
+
+                if root[0][5][0][ergebnis][1][0][0][1].tag == '{http://www.vdv.de/trias}StopPointName':
+                    haltestelle = root[0][5][0][ergebnis][1][0][0][1][0].text.replace("Ã¶", "ö").replace("Ã", "ß").replace("Ã¼",
+                                                                                                                     "ü").replace(
+                        "Ã", "Ö").replace("Ã¤", "ä")
+                else:
+                    print("Haltestelle nicht verfügbar")
+
+                # Element Tree für die prognostizierte Abfahrtszeit
+                if len(root[0][5][0][ergebnis][1][0][0][2]) == 2:
+                    estimatedtime = root[0][5][0][ergebnis][1][0][0][2][1].text + "h"
+                    # Berechnung der Verspätung, Erfassung des Datums und Erstellung des Doppelcodes (ID)
+                    if len(estimatedtime) & len(timtabletime) == 21:
+                        estdt = datetime.strptime(estimatedtime, '%Y-%m-%dT%H:%M:%SZh')
+                        tbldt = datetime.strptime(timtabletime, '%Y-%m-%dT%H:%M:%SZh')
+                        diff = (estdt - tbldt)
+                        seconds = diff.total_seconds()
+                        minutes = seconds / 60
+                        verspaetung = int(minutes)
+                        tbl1 = timtabletime.split("T")
+                        tbl2 = tbl1[1].split("Z")
+                        est1 = estimatedtime.split("T")
+                        est2 = est1[1].split("Z")
+                        ts = time.localtime()
+                        currentdate = time.strftime("%Y-%m-%d", ts)
+                        datecode = time.strftime("%Y%m%d", ts)
+                        doppelcode = str(datecode + tbl2[0] + linie + richtung)
+
+                        print(haltestelle)
+                        print(linie)
+                        print(timtabletime)
+                        print(estimatedtime)
+                        print(richtung)
+                        print(verspaetung)
+
+                        if verspaetung != 0:
+                            connection = mysql.connector.connect(host="35.246.241.173", user="root", passwd="root",
+                                                                 db="vvscloud")
+                            print("Connection ist da")
+                            curser = connection.cursor()
+                            print("Curser ist da")
+
+                            curser = connection.cursor()
+                            curser.execute(
+                                "INSERT IGNORE INTO ubahndaten (id,datum,estimated,timetabled,verspaetung,haltestelle,zeitstempel) VALUES (%s,%s,%s,%s,%s,%s,%s)",
+                                (doppelcode, currentdate, est2[0], tbl2[0], verspaetung, haltestelle, tbldt))
+                            curser.close()
+                            connection.commit()
+                            print("In Datenbank geschrieben")
+                        else:
+                            print("Keine Verspätung, kein Datenbankeintrag")
+                else:
+                    print("Probleme bei der Echtzeit-Erfassung auf dem Server")
+                ergebnis = ergebnis +1
+        else:
+            print("Probleme bei der API-Abfrage: Liefert ERROR-Meldung zurück")
---
+++ producer2.py
@@ -0,0 +1,61 @@
+import requests
+import time
+import pykafka
+from pykafka import KafkaClient
+import csv
+
+client = KafkaClient(hosts="localhost:9092")
+topic = client.topics['ubahnV1']
+producer = topic.get_sync_producer()
+
+csv = csv.reader(open("Haltestellen.csv","r"), delimiter=";")
+list = []
+list.extend(csv)
+namen = []
+
+for data in list:
+    namen.append(data[1])
+
+headers = {'User-Agent': 'Fiddler', 'Host': 'efastatic.vvs.de', 'content-type': 'text/xml',
+           "Keep-Alive": "timeout=20, max=100"}
+while True:
+    ts = time.localtime()
+    currenttime = time.strftime("%Y-%m-%dT%H:%M:%S", ts)
+
+    for stops in namen:
+        xml = '''<?xml version="1.0" encoding="UTF-8"?>
+        <Trias version="1.1" xmlns="http://www.vdv.de/trias" xmlns:siri="http://www.siri.org.uk/siri">
+        <ServiceRequest>
+        <siri:RequestTimestamp>2020-06-18T14:00:00</siri:RequestTimestamp>
+        <siri:RequestorRef>hdm0419</siri:RequestorRef>
+        <RequestPayload>
+        <StopEventRequest>
+        <Location>
+        <LocationRef>
+        <StopPointRef>de:08111:28</StopPointRef>
+        </LocationRef>
+        <DepArrTime>''' + currenttime + '''</DepArrTime>
+        </Location>
+        <Params>
+        <NumberOfResults>3</NumberOfResults>
+        <StopEventType>departure</StopEventType>
+        <PtModeFilter>
+        <Exclude>false</Exclude>
+        <MetroSubmode>urbanRailway</MetroSubmode>
+        </PtModeFilter>
+        <IncludeRealtimeData>true</IncludeRealtimeData>
+        </Params>
+        </StopEventRequest>
+        </RequestPayload>
+        </ServiceRequest>
+        </Trias>'''
+        r = requests.post("http://efastatic.vvs.de/hdmstuttgart/trias", data=xml, headers=headers).text
+        print (stops)
+        message = r.encode()
+        print ("Encoder fertig")
+        producer.produce(message)
+        print("Ist im Producer")
+        time.sleep(60)
+
+
+    time.sleep(60)
